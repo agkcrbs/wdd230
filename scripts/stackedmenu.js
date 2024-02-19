32386cc44da44c0332ac00336a8e76d1bd88e3d7
agkcrbs <agkcrbs@hotmail.com>
2024-02-19 22:48:52 +0900

    Updated portal site.

--- scripts/stackedmenu.js
+++ scripts/stackedmenu.js
@@ -1,7 +1,9 @@
 const menuButton = document.querySelector("#menu");
 const navigationElement = document.querySelector("nav");
+const mainElement = document.querySelector("main");
 
 menuButton.addEventListener("click", function () {
 	navigationElement.classList.toggle("open");
 	menuButton.classList.toggle("open");
+	mainElement.classList.toggle("open");
 });

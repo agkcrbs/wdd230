9292e5abc640b693adb13f06dfc556575f6d8282
agkcrbs <agkcrbs@hotmail.com>
2024-02-25 16:56:09 +0900

    Updated chamber page.

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,7 @@
+function hoverOver(img) {
+    img.src = "images/silhouette2b.jpg"; // Change the image source on hover
+}
+
+function hoverOff(img) {
+    img.src = "images/silhouette1b.jpg"; // Restore the original image source on mouseout
+}
